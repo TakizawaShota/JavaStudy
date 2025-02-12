a7bbe5d490e7d68befb6cb39b1d0052df720fdef
Takizawa <may.may.rain.555@gmail.com>
2025-02-12 21:25:10 +0900

    プログラムの説明コメントを追記

--- JavaStudy/src/comment/Main.java
+++ JavaStudy/src/comment/Main.java
@@ -1,9 +1,23 @@
 package comment;
 
+/* 同パッケージ内の他クラスファイルからメソッドを呼び出すテスト
+ * クラス名の指定方法も２通りで実装
+ * 
+ * 某時代劇のようなワンシーンを再現しており、
+ * 
+ * 　黄金色の菓子でござる。
+ * →この老いぼれの目はごまかせませんぞ
+ * →ええい。こしゃくな！であえい！
+ * →もういいでしょう
+ * →この紋所が目にはいらぬか！
+ * →この老いぼれの目はごまかせませんぞ
+ * とセリフ表示を、それぞれ前半(Zenhan)後半(Kouhan)に分けて実装。
+ * */
+
 public class Main {
 
 	public static void main(String[] args)throws Exception {
-		// TODO 自動生成されたメソッド・スタブ
+	
 		Zenhan.doWarusa();
 		Zenhan.doTogame();
 		
