e9f779c02fd4ed2effdfe30725758db85166a418
Takizawa <may.may.rain.555@gmail.com>
2025-02-12 21:10:40 +0900

    プログラムの説明コメントを追加

--- JavaStudy/src/areaCalculation/AreaCalculation.java
+++ JavaStudy/src/areaCalculation/AreaCalculation.java
@@ -2,7 +2,15 @@ package areaCalculation;
 
 public class AreaCalculation {
 
+	/* メインメソッド外部にメソッドを作成し、メインメソッド内で呼び出すことを目的として作成
+	 * 
+	 * 三角形の面積を求めるcalcTriangleArea、
+	 * 円の面積を求めるcalcsaCircleAreaをそれぞれ作成し
+	 * 問題なく動作するか確かめた。
+	 */
+	
 	public static void main(String[] args) {
+		
 		
 	double tirangleArea = calcTriangleArea(10.0, 5.0);
 	double circleArea = calcsaCircleArea(5.0);
@@ -11,7 +19,6 @@ public class AreaCalculation {
 	
 	System.out.println("円の面積"+circleArea);
 	
-
 	}
 	
 	//三角形の面積を計算するメソッド
